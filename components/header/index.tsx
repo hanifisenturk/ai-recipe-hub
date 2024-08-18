6e067ee9a7742cf4d6a7c3000061985d2017362f
Mehmet Şentürk <mehmet.senturk@akinon.com>
2024-08-18 19:37:44 +0300

    feat(ui): header added

---
+++ components/header/index.tsx
@@ -0,0 +1,19 @@
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+export const Header = () => {
+  return (
+    <header className="h-20 border-b border-white border-opacity-10 flex items-center">
+      <div className="max-w-[1140px] w-full mx-auto flex items-center justify-between">
+        <h1 className="text-3xl">Recipe Hub</h1>
+        <Input
+          placeholder="Search recipes"
+          className="w-96 focus-visible:ring-1 focus-visible:ring-orange-600 transition-all"
+        />
+        <div className="flex gap-4">
+          <Button variant={"ghost"}>Login</Button>
+        </div>
+      </div>
+    </header>
+  );
+};
