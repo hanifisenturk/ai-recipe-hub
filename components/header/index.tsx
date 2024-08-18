9b86fdcbc3b524c97be49b7f89e6f339c86b3139
Mehmet Şentürk <mehmet.senturk@akinon.com>
2024-08-18 19:38:28 +0300

    fix(ui): header link  added

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "lucide-react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 
@@ -5,7 +6,9 @@ export const Header = () => {
   return (
     <header className="h-20 border-b border-white border-opacity-10 flex items-center">
       <div className="max-w-[1140px] w-full mx-auto flex items-center justify-between">
-        <h1 className="text-3xl">Recipe Hub</h1>
+        <Link href="/" className="text-3xl">
+          Recipe Hub
+        </Link>
         <Input
           placeholder="Search recipes"
           className="w-96 focus-visible:ring-1 focus-visible:ring-orange-600 transition-all"
